fcc28a04b7cac9719fb3d07fac9fea5a9bacec13
krzyfok <117074791+krzyfok@users.noreply.github.com>
2025-04-02 21:33:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
+#include <iostream>
 #include <thread>
-#include<mutex>
-#include<vector>
-#include <stdlib.h> 
-#include<condition_variable>
+#include <mutex>
+#include <vector>
+#include <condition_variable>
+#include <cstdlib>
+
 using namespace std;
 
-#define n 10
 vector<thread> ThreadVector;
 mutex mtx;
-vector<condition_variable> varPhi(n);
-vector<int> status(n,2);
+vector<condition_variable> varPhi;
+vector<int> status;
+int num_philosophers;
 
 void dining(int id)
 {
@@ -18,64 +19,75 @@ void dining(int id)
     {
         this_thread::sleep_for(chrono::milliseconds(1000));
         {
-           
             unique_lock<mutex> lock(mtx);
-            status[id]=1;
+            status[id] = 1;
 
-            while (status[(id+1)%n]==0 || status[(id+n-1)%n]==0)
+            while (status[(id + 1) % num_philosophers] == 0 || status[(id + num_philosophers - 1) % num_philosophers] == 0)
             {
                 varPhi[id].wait(lock);
             }
-            
-           status[id]=0;
+
+            status[id] = 0;
         }
-        
 
         this_thread::sleep_for(chrono::milliseconds(1000));
         {
-
             lock_guard<mutex> lock(mtx);
-            status[id]=2;
+            status[id] = 2;
 
-            varPhi[(id+1)%n].notify_one();
-            varPhi[(id+n-1)%n].notify_one();
-
+            varPhi[(id + 1) % num_philosophers].notify_one();
+            varPhi[(id + num_philosophers - 1) % num_philosophers].notify_one();
         }
     }
-    
-    
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     
-   for(int i=0;i<n;i++)
-   {
-    ThreadVector.emplace_back(dining,i);
-   }
 
-   
-   while (true)
+    num_philosophers = atoi(argv[1]);
+    
+
+    status.resize(num_philosophers, 2);
+    varPhi = vector<condition_variable>(num_philosophers);
+
+    for (int i = 0; i < num_philosophers; i++)
     {
+        ThreadVector.emplace_back(dining, i);
+    }
+
+    while (true)
+    {
+#ifdef _WIN32
         system("cls");
-       
-        for (int i = 0; i < n; i++) {
-            if (status[i] == 0) {
+#else
+        system("clear");
+#endif
+        for (int i = 0; i < num_philosophers; i++)
+        {
+            if (status[i] == 0)
+            {
                 cout << "PHILOSOPHER: " << i << " IS EATING" << endl;
-            } else if (status[i] == 1) {
+            }
+            else if (status[i] == 1)
+            {
                 cout << "PHILOSOPHER: " << i << " IS HUNGRY" << endl;
-            } else {
+            }
+            else
+            {
                 cout << "PHILOSOPHER: " << i << " IS THINKING" << endl;
             }
         }
         this_thread::sleep_for(chrono::milliseconds(500));
     }
-   for (auto &th : ThreadVector) {
-    if (th.joinable()) {
-        th.join();
+
+    for (auto &th : ThreadVector)
+    {
+        if (th.joinable())
+        {
+            th.join();
+        }
     }
-    }
-    
-    
+
     return 0;
 }
